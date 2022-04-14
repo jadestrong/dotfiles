d9d2a8ac1ce4577a4793821c28ccb909beefe632
zhangyuqiang03 <zhangyuqiang03@kuaishou.com>
2022-04-14 16:24:38 +0800

    feat: update fish

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -21,26 +21,13 @@ set -gx LANG en_US.UTF-8
 set -gx PATH ~/.emacs.d/bin $PATH # doom command
 set -gx PATH ~/.cargo/bin $PATH # rust
 set -gx PATH /Applications/Firefox.app/Contents/MacOS $PATH
-set -gx PATH /run/current-system/sw/bin $PATH # All nix commond
 set -gx PATH ~/bin $PATH # system
 set -gx PATH /usr/local/opt/make/libexec/gunbin $PATH
-fenv source '$HOME/.nix-profile/etc/profile.d/nix.sh'
 set -gx PATH /usr/local/Cellar/universal-ctags/HEAD-c436bca/bin $PATH # must below nix.sh, it need override ctags from nix emacs
-set -gx NIX_PATH darwin-config=$HOME/.nixpkgs/darwin-configuration.nix
-set -gx NIX_PATH $HOME/.nix-defexpr/channels $NIX_PATH
-
 set -g fish_user_paths "/usr/local/opt/luajit-openresty/bin" $fish_user_paths
 
 [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
-starship init fish | source
-
-# export PATH="$HOME/.emacs.d/bin:$HOME/.cargo/bin:/run/current-system/sw/bin:$HOME/bin:/usr/local/opt/make/libexec/gnubin:$PATH"
-# export LANG=en_US.UTF-8
-
-
-# function cheat.sh
-#     curl cheat.sh/$argv
-# end
+# starship init fish | source
 
 # NVM
 function __check_rvm --on-variable PWD --description 'Do nvm stuff'
