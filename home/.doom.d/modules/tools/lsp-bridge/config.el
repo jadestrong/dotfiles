d60cf3d616cff1b8b20c7591da0cf85fd3d5d262
zhangyuqiang03 <zhangyuqiang03@kuaishou.com>
2022-06-03 11:15:29 +0800

    update lsp-bridge

--- home/.doom.d/modules/tools/lsp-bridge/config.el
+++ home/.doom.d/modules/tools/lsp-bridge/config.el
@@ -9,12 +9,9 @@
   (setq corfu-preview-current nil)
   (setq corfu-preselect-first t)
   (setq corfu-on-exact-match 'quit)
-  (add-to-list 'lsp-bridge-enable-popup-predicates
-               '((lambda ()
-                   (and
-                    (< corfu--index 0) ; not select a candidate
-                    (or (not (featurep 'evil)) (evil-insert-state-p))))))
   (global-corfu-mode))
+
+(use-package! corfu-doc)
 
 (use-package! lsp-bridge
   :init
@@ -22,8 +19,20 @@
   (require 'lsp-bridge-icon)
   :config
   (setq lsp-bridge-enable-log nil)
+  ;; (setq lsp-bridge-enable-auto-import t)
   (setq lsp-bridge-completion-stop-commands '(corfu-complete corfu-insert undo-tree-undo undo-tree-redo save-buffer evil-normal-state))
+  (set-lookup-handlers! 'lsp-bridge-mode
+    :definition #'lsp-bridge-find-def
+    :references #'lsp-bridge-find-references
+    :documentation #'lsp-bridge-lookup-documentation
+    :implementations #'lsp-bridge-find-impl)
+
   (global-lsp-bridge-mode))
+
+(use-package! lsp-bridge-diagnostics
+  :after (lsp-bridge)
+  :config
+  (setq lsp-bridge-diagnostics-provider :flycheck))
 
 (defun lsp-bridge--vue-project-p (project-path)
   (if-let ((package-json (f-join project-path "package.json"))
@@ -33,14 +42,23 @@
       (alist-get 'vue dependencies)
     nil))
 
-(setq vue-project-list '(
+(defun my/get-private-langserver (file-name)
+  (f-join doom-private-dir "langserver" file-name))
+
+(setq volar-project-list '(
                          "/media/psf/Home/Documents/JadeStrong/olympic-game"
                          "/Users/jadestrong/Documents/JadeStrong/ugfe-activity"
                          ))
+(setq vls-project-list '(
+                         "/Users/jadestrong/Documents/JadeStrong/ugfe-nebula"
+                         ))
 (defun lsp-bridge--get-lang-server-by-project (project-path file-path)
   "Get lang server by PROJECT-PATH ANF FILE_PATH."
-  (if (or (member project-path vue-project-list)
-          (lsp-bridge--vue-project-p project-path))
-      "/Users/jadestrong/.doom.d/langserver/volar.json"
-    nil))
+  (cond ((and (member project-path vls-project-list)
+              (string-equal (file-name-extension file-path) "vue"))
+         (my/get-private-langserver "vls.json"))
+        ((or (member project-path volar-project-list)
+             (and (not (member project-path vls-project-list)) (lsp-bridge--vue-project-p project-path)))
+         (my/get-private-langserver "volar.json"))
+        (t nil)))
 (setq lsp-bridge-get-lang-server-by-project #'lsp-bridge--get-lang-server-by-project)
--- home/.doom.d/modules/tools/lsp-bridge/packages.el
+++ home/.doom.d/modules/tools/lsp-bridge/packages.el
@@ -1,5 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/lsp-bridge/packages.el
 (package! lsp-bridge :recipe (:host github :repo "jadestrong/lsp-bridge" :files ("*.el" "*.py" "core/*.py" "langserver/*.json")))
+(package! lsp-bridge-diagnostics :recipe (:host github :repo "jadestrong/lsp-bridge-diagnostics"))
 (package! corfu :recipe (:host github :repo "minad/corfu" :files ("*.el" "extensions/*.el")))
+(package! corfu-doc :recipe (:host github :repo "galeo/corfu-doc"))
 (package! orderless)
