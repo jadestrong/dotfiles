f01f1b00430a792fd9e69f256db7c7f3197d2e57
zhangyuqiang03 <zhangyuqiang03@kuaishou.com>
2022-06-14 14:32:10 +0800

    fix: god-mode support emacs-rime

--- home/.doom.d/modules/editor/god-state/config.el
+++ home/.doom.d/modules/editor/god-state/config.el
@@ -9,4 +9,13 @@
   :after (god-mode evil)
   :config
   (evil-define-key 'normal global-map "," 'evil-execute-in-god-state)
-  (evil-define-key 'god global-map [escape] 'evil-god-state-bail))
+  (evil-define-key 'god global-map [escape] 'evil-god-state-bail)
+  ;; god-mode support emacs-rime
+  (defadvice! +rime-predicate-evil-mode-p ()
+    :override #'rime-predicate-evil-mode-p
+    (and (fboundp 'evil-mode)
+       (or (evil-normal-state-p)
+           (evil-visual-state-p)
+           (evil-motion-state-p)
+           (evil-god-state-p)
+           (evil-operator-state-p)))))
